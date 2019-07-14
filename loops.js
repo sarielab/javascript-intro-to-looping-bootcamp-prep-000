be910abe97e208adaae3fc09c35090f055d0060d
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-14 17:39:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,7 +13,7 @@ var forLoop = (array) => {
   const newArray = []
   const arrLen = array.length
   for(let i = -2; i< 25; i++) {
-    newArray.push(`I am ${i} strange loop${i < 3? '' : 's'}.`)
+    newArray.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
   }
   return newArray
 }
